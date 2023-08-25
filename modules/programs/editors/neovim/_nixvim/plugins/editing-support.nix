a9e13111e7ebd7bf1edd96db0d0344b1de369991
wolbyte <wolbyte@gmail.com>
2023-08-25 13:25:45 +0330

    modules(neovim/plugins/editing-support): init spider + leap

--- modules/programs/editors/neovim/_nixvim/plugins/editing-support.nix
+++ modules/programs/editors/neovim/_nixvim/plugins/editing-support.nix
@@ -1,11 +1,26 @@
-let
-  helpers = import ../helpers.nix;
-in {
+{
   programs.nixvim = {
     plugins = {
       comment-nvim = {
         enable = true;
       };
+
+      spider = {
+        enable = true;
+
+        skipInsignificantPunctuation = true;
+
+        keymaps = {
+          motions = {
+            w = "w";
+            e = "e";
+            b = "b";
+            ge = "ge";
+          };
+        };
+      };
+
+      leap.enable = true;
     };
   };
 }
