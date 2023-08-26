a4e2634087f68bd96fbb671615e352b705c91add
wolbyte <wolbyte@gmail.com>
2023-08-26 21:13:53 +0330

    modules(neovim/languages/cpp): enable inlayhints + debugging support

--- modules/programs/editors/neovim/_nixvim/languages/nix.nix
+++ modules/programs/editors/neovim/_nixvim/languages/nix.nix
@@ -1,9 +1,10 @@
-{pkgs, ...}: {
+{
   programs.nixvim = {
     plugins = {
       nix.enable = true;
 
       lsp.servers.nil_ls.enable = true;
+
       null-ls.sources = {
         code_actions.statix.enable = true;
         diagnostics.statix.enable = true;
