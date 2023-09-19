3e0b62582fc87e0c103f6a2b4458dcda89f87073
wolbyte <wolbyte@gmail.com>
2023-09-19 20:58:40 +0330

    overlays(spotify): update to 1.2.13.661.ga588f749

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,11 @@
 {
   modifications = self: super: {
     spotify = super.spotify.overrideAttrs (old: rec {
-      version = "1.2.11.916.geb595a67";
-      rev = "67";
+      version = "1.2.13.661.ga588f749";
+      rev = "68";
       src = super.fetchurl {
         url = "https://api.snapcraft.io/api/v1/snaps/download/pOBIoZ2LrCB3rDohMxoYGnbN14EHOgD7_${rev}.snap";
-        sha256 = "sha256-v77yoVea1Eec2KCqIXMfa1aLuKP/ghX9lizzIYX8DSk=";
+        sha256 = "sha256-UbA0HHLCnENO3e3c+wOeuJ9F9d19S5MAQ6zEg2hitwk=";
       };
       unpackPhase = builtins.replaceStrings ["${old.version}"] ["${version}"] old.unpackPhase;
     });
