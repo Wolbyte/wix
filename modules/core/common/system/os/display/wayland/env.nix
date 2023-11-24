86bb2ca8f4e6097afc782a8d5d2bdc60fea2347c
wolbyte <wolbyte@gmail.com>
2023-11-24 22:23:22 +0330

    modules(core/wayland): rename NIXOS_OZONE_LAYER to NIXOS_OZONE_WL

--- modules/core/common/system/os/display/wayland/env.nix
+++ modules/core/common/system/os/display/wayland/env.nix
@@ -11,7 +11,7 @@ in {
     environment = {
       variables = {
         _JAVA_AWT_WM_NONEREPARENTING = "1";
-        NIXOS_OZONE_LAYER = "1";
+        NIXOS_OZONE_WL = "1";
 
         CLUTTER_BACKEND = "wayland";
         GDK_BACKEND = "wayland,x11";
