2c86aee630d26529bf05331b716355e9a4ba856c
wolbyte <wolbyte@gmail.com>
2023-12-05 17:44:29 +0330

    homes(wolbyte/neovim): add window movement maps in terminal mode

--- homes/wolbyte/terminal/editors/neovim/mappings.nix
+++ homes/wolbyte/terminal/editors/neovim/mappings.nix
@@ -26,6 +26,17 @@ in {
         "<leader>C" = mkLuaMap "function() require('mini.bufremove').delete(0, true) end" "Close Buffer (Force)";
       };
 
+      t = {
+        "<C-h>" = mkLuaMap "require('smart-splits').move_cursor_left" "Move to left split";
+        "<C-j>" = mkLuaMap "require('smart-splits').move_cursor_down" "Move to below split";
+        "<C-k>" = mkLuaMap "require('smart-splits').move_cursor_up" "Move to above split";
+        "<C-l>" = mkLuaMap "require('smart-splits').move_cursor_right" "Move to right split";
+        "<C-Up>" = mkLuaMap "require('smart-splits').resize_up" "Resize split up";
+        "<C-Down>" = mkLuaMap "require('smart-splits').resize_down" "Resize split down";
+        "<C-Left>" = mkLuaMap "require('smart-splits').resize_left" "Resize split left";
+        "<C-Right>" = mkLuaMap "require('smart-splits').resize_right" "Resize split right";
+      };
+
       v = {
         "<Tab>" = mkRawMap ">gv" "Indent selected lines";
         "<S-Tab>" = mkRawMap "<gv" "Unindent selected lines";
