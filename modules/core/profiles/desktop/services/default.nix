d2b3c2be7779e26842e0b64e1a1ecb3d27c41788
wolbyte <wolbyte@gmail.com>
2024-03-19 23:32:50 +0330

    profiles(desktop/services/login): init

--- modules/core/profiles/desktop/services/default.nix
+++ modules/core/profiles/desktop/services/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./gnome.nix
+    ./login.nix
     ./misc.nix
   ];
 }
---
+++ modules/core/profiles/desktop/services/login.nix
@@ -0,0 +1,60 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  sessions = config.services.xserver.displayManager.sessionData.desktops;
+  sessionsPath = concatStringsSep ":" [
+    "${sessions}/share/xsessions"
+    "${sessions}/share/wayland-sessions"
+  ];
+in {
+  config = {
+    # unlock GPG keyring on login
+    security.pam.services = {
+      login = {
+        enableGnomeKeyring = true;
+        gnupg = {
+          enable = true;
+          noAutostart = true;
+          storeOnly = true;
+        };
+      };
+
+      greetd = {
+        gnupg.enable = true;
+        enableGnomeKeyring = true;
+      };
+    };
+
+    services = {
+      xserver.displayManager.session = [
+        {
+          manage = "desktop";
+          name = "Hyprland";
+          start = "Hyprland";
+        }
+      ];
+
+      greetd = {
+        enable = true;
+        vt = 2;
+        settings = {
+          default_session = {
+            user = "greeter";
+            command = concatStringsSep " " [
+              (getExe pkgs.greetd.tuigreet)
+              "--time"
+              "--remember"
+              "--remember-user-session"
+              "--asterisks"
+              "--sessions '${sessionsPath}'"
+            ];
+          };
+        };
+      };
+    };
+  };
+}
