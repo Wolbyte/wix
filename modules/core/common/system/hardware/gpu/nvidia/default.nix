80ede6ea8e01174aea778ab8ab8a593cfed39dcc
wolbyte <wolbyte@gmail.com>
2024-05-06 15:50:26 +0330

    modules(hardware/nvidia): refactor

--- modules/core/common/system/hardware/gpu/nvidia/default.nix
+++ modules/core/common/system/hardware/gpu/nvidia/default.nix
@@ -8,63 +8,80 @@ with lib;
 with lib.wb; let
   inherit (config.wb) device env;
 
-  nvStable = config.boot.kernelPackages.nvidiaPackages.stable;
-  nvBeta = config.boot.kernelPackages.nvidiaPackages.beta;
-  nvPackage =
-    if (versionOlder nvBeta.version nvStable.version)
-    then nvStable
-    else nvBeta;
+  stableDriver = config.boot.kernelPackages.nvidiaPackages.stable;
+
+  betaDriver = config.boot.kernelPackages.nvidiaPackages.beta;
+
+  driverPackage =
+    if (versionOlder betaDriver.version stableDriver.version)
+    then stableDriver
+    else betaDriver;
 in {
   config = mkIf (device.gpu == "nvidia" || device.gpu == "hybrid-nv") {
     nixpkgs.config.allowUnfree = true;
 
     services.xserver.videoDrivers = ["nvidia"];
+
     boot.blacklistedKernelModules = ["nouveau"];
+
+    hardware = {
+      nvidia = {
+        package = mkDefault driverPackage;
+
+        modesetting.enable = mkDefault true;
+
+        prime.offload.enableOffloadCmd = device.gpu == "hybrid-nv";
+
+        powerManagement = {
+          enable = mkDefault true;
+
+          finegrained = mkDefault device.gpu == "hybrid-nv";
+        };
+
+        open = mkDefault true;
+
+        nvidiaPersistenced = true;
+
+        forceFullCompositionPipeline = true;
+      };
+
+      opengl = {
+        extraPackages = with pkgs; [nvidia-vaapi-driver];
+
+        extraPackages32 = with pkgs.pkgsi686Linux; [nvidia-vaapi-driver];
+      };
+    };
 
     environment = {
       sessionVariables = mkMerge [
         {LIBVA_DRIVER_NAME = "nvidia";}
 
         (mkIf env.isWayland {
-          WLR_NO_HARDWARE_CURSORS = "1";
+          NVD_BACKEND = "direct";
           __GLX_VENDOR_LIBRARY_NAME = "nvidia";
           GBM_BACKEND = "nvidia-drm";
+        })
+
+        (mkIf (env.desktopEnv != "Hyprland") {
+          # Hardware cursor is fixed in wlroots-hyprland
+          WLR_NO_HARDWARE_CURSORS = "1";
         })
       ];
 
       systemPackages = with pkgs; [
+        # Mesa
         mesa
 
+        # Vulkan
         vulkan-tools
         vulkan-loader
         vulkan-validation-layers
         vulkan-extension-layer
 
+        # VA-API
         libva
         libva-utils
       ];
-    };
-
-    hardware = {
-      nvidia = {
-        package = mkDefault nvPackage;
-        modesetting.enable = mkDefault true;
-        prime.offload.enableOffloadCmd = device.gpu == "hybrid-nv";
-        powerManagement = {
-          enable = mkDefault true;
-          finegrained = mkDefault device.gpu == "hybrid-nv";
-        };
-
-        # TODO: change to true once nvidia-open works with vaapi
-        open = mkDefault false;
-        nvidiaPersistenced = true;
-        forceFullCompositionPipeline = true;
-      };
-
-      opengl = {
-        extraPackages = with pkgs; [nvidia-vaapi-driver];
-        extraPackages32 = with pkgs.pkgsi686Linux; [nvidia-vaapi-driver];
-      };
     };
   };
 }
