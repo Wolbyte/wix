20153a19c38166caff665ce3e85f4a16447e76e9
wolbyte <wolbyte@gmail.com>
2025-04-12 21:23:47 +0330

    pipewire: disable sink suspension

--- modules/nixos/media/audio.nix
+++ modules/nixos/media/audio.nix
@@ -15,13 +15,30 @@ with lib;
     services.pipewire = {
       enable = true;
 
-      wireplumber.enable = true;
-
       pulse.enable = true;
 
       jack.enable = true;
 
       alsa.enable = true;
+
+      wireplumber = {
+        enable = true;
+
+        configPackages = [
+          (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/99-disable-suspension.conf" ''
+            monitor.alsa.rules = [
+              {
+                matches = [ { node.name = "~alsa_output.*" } ]
+                actions = {
+                  update-props = {
+                    session.suspend-timeout-seconds = 0
+                  }
+                }
+              }
+            ]
+          '')
+        ];
+      };
     };
 
     systemd.user.services = {
