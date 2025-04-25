afc8a0347f11684ee17db9549de25c24f2f9af6b
wolbyte <wolbyte@gmail.com>
2025-04-25 17:10:24 +0330

    dolphin: init

--- homes/wolbyte/programs/default.nix
+++ homes/wolbyte/programs/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./browsers
     ./cli
+    ./filemanagers
     ./media
     ./rofi
     ./shell
---
+++ homes/wolbyte/programs/filemanagers/dolphin.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  lib,
+  osConfig,
+  ...
+}:
+let
+  inherit (osConfig.wix) host;
+in
+with lib;
+{
+  config = mkIf (host.enableDesktopFeatures) {
+    home.packages = with pkgs; [
+      kdePackages.dolphin
+
+      # images, pdf, blender files
+      kdePackages.kdegraphics-thumbnailers
+      # webp, tiff, tga, jp2 files
+      kdePackages.qtimageformats
+      # video files
+      kdePackages.ffmpegthumbs
+      # audio files
+      kdePackages.taglib
+      # appimage embedded icons
+      libappimage
+      # anything ico related
+      icoutils
+    ];
+  };
+}
